bbc90cf67b4e8865ff64f777b9c32f1c799cab65
haukzero <haukzero1@gmail.com>
2025-12-03 12:51:27 +0800

    chore(release): bump version to v3.0.4

--- include/common.h
+++ include/common.h
@@ -6,7 +6,7 @@
 namespace punp {
 
     namespace Version {
-        constexpr const char *VERSION = "3.0.3";
+        constexpr const char *VERSION = "3.0.4";
     } // namespace Version
 
     namespace RuleFile {
